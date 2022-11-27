5ea5a8302a9313c6fad017e11da0a5ff004b400a
MichaelSoquat <michael.soquat@gmx.de>
2022-11-27 08:32:45 +0100

    added validators

--- routes/feed.js
+++ routes/feed.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const { body } = require('express-validator');
 const router = express.Router();
 
 const feedController = require('../controllers/feed');
 
-
+// GET POSTS
 router.get('/posts', feedController.getPosts);
-router.post('/posts', feedController.createPost);
+
+// POST POST
+router.post('/post', [
+    body('title').trim().isLength({ min: 5 }),
+    body('content').trim().isLength({ min: 5 })
+
+], feedController.createPost);
+
+
+
+
 
 
 
